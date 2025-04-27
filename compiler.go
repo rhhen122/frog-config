6c0010cf5b922342a6d27d78b2a98260586115fd
RHHEN122 <rokyhenderson222@gmail.com>
2025-04-28 11:57:40 +1200

    Added some basic code

---
+++ compiler.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+var usrinput string
+
+func start() {
+	fmt.Println("miku-go")
+	fmt.Println("A Compiler built for turning .frog files into valid python configs")
+	fmt.Println("===============")
+	fmt.Print("")
+}
+
+func read() {
+	for i := 0; i < len(mystr); i++ {
+		
+	}
+}
+
+func main() {
+	start()
+	fmt.Scan(&usrinput)
+}
